dd6a728530824b28fdf3fa909c9444dc402502a9
HuyNguyen <huynguyencongbka@gmail.com>
2024-06-28 22:42:40 +0700

    This is the second commit
    This is the second commit
    
    Co-Authored-By: Huy Nguyen <weehuy@gmail.com>

---
+++ 2PortsAndTwoDataGridviews/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace _2PortsAndTwoDataGridviews
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.GR2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GR1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnSaveToExcel = new System.Windows.Forms.Button();
+            this._serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this._serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.GR2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.GR1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 546);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1013, 57);
+            this.panel2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(696, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(314, 22);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Email: huynguyencongbka@gmail.com";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(745, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(265, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "CREATED BY: HUY NGUYEN";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1013, 56);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1010, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "READ DATA FROM TWO COM PORTS RS232 AND PUSH TO SQL DATABASE";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 56);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1013, 490);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.GR2);
+            this.panel5.Controls.Add(this.GR1);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(306, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(707, 490);
+            this.panel5.TabIndex = 1;
+            // 
+            // GR2
+            // 
+            this.GR2.Controls.Add(this.dataGridView2);
+            this.GR2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.GR2.Location = new System.Drawing.Point(363, 0);
+            this.GR2.Name = "GR2";
+            this.GR2.Size = new System.Drawing.Size(344, 490);
+            this.GR2.TabIndex = 0;
+            this.GR2.TabStop = false;
+            this.GR2.Text = "COM24";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3,
+            this.Column4});
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 18);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(338, 469);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "DATE";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "DATA";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // GR1
+            // 
+            this.GR1.Controls.Add(this.dataGridView1);
+            this.GR1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.GR1.Location = new System.Drawing.Point(0, 0);
+            this.GR1.Name = "GR1";
+            this.GR1.Size = new System.Drawing.Size(362, 490);
+            this.GR1.TabIndex = 0;
+            this.GR1.TabStop = false;
+            this.GR1.Text = "COM22";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 18);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(356, 469);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "DATE";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "DATA";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btnSaveToExcel);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(306, 490);
+            this.panel4.TabIndex = 0;
+            // 
+            // btnSaveToExcel
+            // 
+            this.btnSaveToExcel.Location = new System.Drawing.Point(68, 35);
+            this.btnSaveToExcel.Name = "btnSaveToExcel";
+            this.btnSaveToExcel.Size = new System.Drawing.Size(148, 61);
+            this.btnSaveToExcel.TabIndex = 0;
+            this.btnSaveToExcel.Text = "Save to Excel";
+            this.btnSaveToExcel.UseVisualStyleBackColor = true;
+            this.btnSaveToExcel.Click += new System.EventHandler(this.btnSaveToExcel_Click);
+            // 
+            // _serialPort1
+            // 
+            this._serialPort1.PortName = "COM22";
+            // 
+            // _serialPort2
+            // 
+            this._serialPort2.PortName = "COM24";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 603);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "COM PORT TO SQL DATABASE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.GR2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.GR1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.GroupBox GR2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.GroupBox GR1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.IO.Ports.SerialPort _serialPort1;
+        private System.IO.Ports.SerialPort _serialPort2;
+        private System.Windows.Forms.Button btnSaveToExcel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ 2PortsAndTwoDataGridviews/Form1.cs
@@ -0,0 +1,277 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+using System.Data.SqlClient;
+
+
+
+namespace _2PortsAndTwoDataGridviews
+{
+    public partial class Form1 : Form
+    {
+        private Thread _initializeThread1;
+        private Thread _initializeThread2;
+        private SqlConnection _sqlConnection;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDatabase();
+            StartInitializationThreads();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void InitializeDatabase()
+        {
+            try
+            {
+                string connectionString = "Server=HuyNguyen\\TONYNGUYEN;Database=READ_DATA_COM_PUSH_TO_SQLSERVER;User Id=sa;Password=Nchbka1999";
+                _sqlConnection = new SqlConnection(connectionString);
+                _sqlConnection.Open(); // Open connection to SQL Server
+                MessageBox.Show("Connected to SQLServer!");
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Failed to connect to SQLServer! " + e.Message);
+                this.Close();
+            }
+        }
+
+        private void StartInitializationThreads()
+        {
+            _initializeThread1 = new Thread(InitializeSerialPort1);
+            _initializeThread2 = new Thread(InitializeSerialPort2);
+
+            _initializeThread1.Start();
+            _initializeThread2.Start();
+        }
+
+        private void InitializeSerialPort1()
+        {
+            _serialPort1 = new SerialPort("COM22", 9600, Parity.None, 8, StopBits.One);
+            _serialPort1.ReadTimeout = 500;
+            _serialPort1.WriteTimeout = 500;
+            _serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler1);
+
+            try
+            {
+                _serialPort1.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error opening COM22: {ex.Message}");
+            }
+        }
+
+
+        private void InitializeSerialPort2()
+        {
+            _serialPort2 = new SerialPort("COM24", 9600, Parity.None, 8, StopBits.One);
+            _serialPort2.ReadTimeout = 500;
+            _serialPort2.WriteTimeout = 500;
+            _serialPort2.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler2);
+
+            try
+            {
+                _serialPort2.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error opening COM23: {ex.Message}");
+            }
+        }
+
+        private void DataReceivedHandler1(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string data = _serialPort1.ReadExisting();
+                string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+
+                this.BeginInvoke(new Action(() =>
+                {
+                    dataGridView1.Rows.Add(timestamp, data);
+                    CheckAndRemoveDuplicates(dataGridView1, 1, 2);
+                }));
+
+                InsertDataIntoDatabase1("COM22", timestamp, data);
+            }
+            catch (TimeoutException) { }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading COM22: {ex.Message}");
+            }
+        }
+
+        private void DataReceivedHandler2(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string data = _serialPort2.ReadExisting();
+                string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+
+                this.BeginInvoke(new Action(() =>
+                {
+                    dataGridView2.Rows.Add(timestamp, data);
+                    CheckAndRemoveDuplicates(dataGridView1, 1, 2);
+                }));
+
+                
+
+                InsertDataIntoDatabase2("COM24", timestamp, data);
+            }
+            catch (TimeoutException) { }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading COM23: {ex.Message}");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_serialPort1 != null && _serialPort1.IsOpen)
+            {
+                _serialPort1.Close();
+            }
+
+            if (_serialPort2 != null && _serialPort2.IsOpen)
+            {
+                _serialPort2.Close();
+            }
+
+            if (_initializeThread1 != null && _initializeThread1.IsAlive)
+            {
+                _initializeThread1.Join(); // Wait for the thread to finish
+            }
+
+            if (_initializeThread2 != null && _initializeThread2.IsAlive)
+            {
+                _initializeThread2.Join(); // Wait for the thread to finish
+            }
+
+            _sqlConnection.Close();
+        }
+
+
+        private void SaveDataToExcel(string filePath)
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                // Add a new worksheet for DataGridView1
+                var worksheet1 = workbook.Worksheets.Add("COM22 Data");
+                for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                {
+                    worksheet1.Cell(1, i + 1).Value = dataGridView1.Columns[i].HeaderText;
+                }
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        worksheet1.Cell(i + 2, j + 1).Value = dataGridView1.Rows[i].Cells[j].Value?.ToString();
+                    }
+                }
+
+                // Add a new worksheet for DataGridView2
+                var worksheet2 = workbook.Worksheets.Add("COM23 Data");
+                for (int i = 0; i < dataGridView2.Columns.Count; i++)
+                {
+                    worksheet2.Cell(1, i + 1).Value = dataGridView2.Columns[i].HeaderText;
+                }
+                for (int i = 0; i < dataGridView2.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView2.Columns.Count; j++)
+                    {
+                        worksheet2.Cell(i + 2, j + 1).Value = dataGridView2.Rows[i].Cells[j].Value?.ToString();
+                    }
+                }
+
+                // Save the Excel file
+                workbook.SaveAs(filePath);
+            }
+
+            MessageBox.Show("Data saved to Excel successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnSaveToExcel_Click(object sender, EventArgs e)
+        {
+            SaveDataToExcel("Z:\\A_Works\\Study\\SelfLearningC#\\WindowFormApps\\NangCao\\2PortsAndTwoDataGridviews\\2PortsAndTwoDataGridviews\\SerialPortData.xlsx");
+        }
+
+        private void InsertDataIntoDatabase1(string portName, string timestamp, string data)
+        {
+            string query = "INSERT INTO DATA_COMPORT1 (COM, DATE, DATA) VALUES (@PortName, @Timestamp, @Data)";
+            SqlCommand command = new SqlCommand(query, _sqlConnection);
+            command.Parameters.AddWithValue("@PortName", portName);
+            command.Parameters.AddWithValue("@Timestamp", timestamp);
+            command.Parameters.AddWithValue("@Data", data);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                Console.WriteLine($"Data inserted into database: Port={portName}, Timestamp={timestamp}, Data={data}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error inserting data into database: {ex.Message}");
+            }
+        }
+
+        private void InsertDataIntoDatabase2(string portName, string timestamp, string data)
+        {
+            string query = "INSERT INTO DATA_COMPORT2 (COM, DATE, DATA) VALUES (@PortName, @Timestamp, @Data)";
+            SqlCommand command = new SqlCommand(query, _sqlConnection);
+            command.Parameters.AddWithValue("@PortName", portName);
+            command.Parameters.AddWithValue("@Timestamp", timestamp);
+            command.Parameters.AddWithValue("@Data", data);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                Console.WriteLine($"Data inserted into database: Port={portName}, Timestamp={timestamp}, Data={data}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error inserting data into database: {ex.Message}");
+            }
+        }
+
+
+        private void CheckAndRemoveDuplicates(DataGridView dataGridView, int columnIndex1, int columnIndex2)
+        {
+            HashSet<string> seenValues = new HashSet<string>();
+
+            for (int i = dataGridView.Rows.Count - 1; i >= 0; i--)
+            {
+                var row = dataGridView.Rows[i];
+                string value1 = row.Cells[columnIndex1].Value?.ToString();
+                string value2 = row.Cells[columnIndex2].Value?.ToString();
+
+                if (value1 != null && value2 != null)
+                {
+                    string combinedValues = value1 + "|" + value2;
+                    if (seenValues.Contains(combinedValues))
+                    {
+                        //dataGridView.Rows.RemoveAt(i);
+                        dataGridView.Rows.
+                    }
+                    else
+                    {
+                        seenValues.Add(combinedValues);
+                    }
+                }
+            }
+        }
+    }
+}
